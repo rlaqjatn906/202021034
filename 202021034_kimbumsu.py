41644999572882286b5dc84dad249533c9fc5ea7
rlaqjatn906 <136241561+rlaqjatn906@users.noreply.github.com>
2023-06-13 00:45:31 +0900

    Update 202021034_kimbumsu.py

--- 202021034_kimbumsu.py
+++ 202021034_kimbumsu.py
@@ -1,10 +1,11 @@
 import streamlit as st
-st.title("202021034 김범수 제어공학")
-st.header("기말고사")
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 import control
+
+st.title("202021034 김범수 제어공학")
+st.header("기말고사")
 
 # 전달함수 정의
 num = [100]
@@ -18,22 +19,24 @@ feedback_tf = control.feedback(sys_tf)
 t, y = control.step_response(feedback_tf)
 
 # 응답곡선 그리기
-plt.figure()
+fig1 = plt.figure()
 plt.plot(t, y)
 plt.title('Step Response')
 plt.xlabel('Time [s]')
 plt.ylabel('Output')
 plt.grid(True)
-plt.show()
 
 # 주파수 응답 계산
 w, mag, phase = control.bode(feedback_tf)
 
 # 보드선도 그리기
-plt.figure()
+fig2 = plt.figure()
 plt.semilogx(w, mag)
 plt.title('Bode Plot')
 plt.xlabel('Frequency [rad/s]')
 plt.ylabel('Magnitude [dB]')
 plt.grid(True)
-plt.show()
+
+# 그래프를 Streamlit 애플리케이션에 표시
+st.pyplot(fig1)
+st.pyplot(fig2)
