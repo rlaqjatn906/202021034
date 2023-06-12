ac8ea45c0f122e24007f06b505298a7c4387c4f0
rlaqjatn906 <136241561+rlaqjatn906@users.noreply.github.com>
2023-06-13 01:03:13 +0900

    Update 202021034_kimbumsu.py

--- 202021034_kimbumsu.py
+++ 202021034_kimbumsu.py
@@ -4,8 +4,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import control
 
-st.title("202021034 김범수 제어공학")
-st.header("기말고사")
+st.title("202021034 김범수 제어공학", style="color: ; font-size: 30px;")
+st.header("기말고사", style="color: blue;")
 
 # 전달함수 정의
 num = [100]
